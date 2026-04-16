0f84cdfd80340b788fbba21cbda0a8523c9d77bc
hiro-stack <zhanghiromo@gmail.com>
2026-04-16 15:33:54 +0900

    イベントへの入門3

--- practice2.js
+++ practice2.js
@@ -1,14 +1,19 @@
-const buttons = document.querySelectorAll("button");
+const btn = document.querySelector("button");
 
 function random(number) {
   return Math.floor(Math.random() * (number + 1));
 }
 
-function changeBackground() {
+function bgChange(e) {
   const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
-  document.body.style.backgroundColor = rndCol;
+  e.target.style.backgroundColor = rndCol;
+  console.log(e);
 }
 
-for (const button of buttons) {
-  button.addEventListener("click", changeBackground);
-}
+btn.addEventListener("click", bgChange);
+
+const textBox = document.querySelector("#textBox");
+const output = document.querySelector("#output");
+textBox.addEventListener("keydown", (event) => {
+  output.textContent = `"${event.key}" が押されました。`;
+});
