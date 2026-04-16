992fa5a183f2ea7a50f85bdbd91cd46538b10c32
hiro-stack <zhanghiromo@gmail.com>
2026-04-16 15:27:35 +0900

    イベントの入門2_複数のボタンへの対応

--- practice2.js
+++ practice2.js
@@ -1,18 +1,14 @@
-const btn = document.querySelector("button");
+const buttons = document.querySelectorAll("button");
 
 function random(number) {
   return Math.floor(Math.random() * (number + 1));
 }
-
-btn.addEventListener("click", () => {
-  const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
-  document.body.style.backgroundColor = rndCol;
-});
-
 
 function changeBackground() {
   const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
   document.body.style.backgroundColor = rndCol;
 }
 
-btn.addEventListener("click", changeBackground);
+for (const button of buttons) {
+  button.addEventListener("click", changeBackground);
+}
